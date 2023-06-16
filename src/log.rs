9cbe2e385f28a9b0476498265b073471a161725c
Noam <22600644+noamteyssier@users.noreply.github.com>
2023-06-15 17:11:38 -0700

    reorganize code to clean up main from logic

--- src/log.rs
+++ src/log.rs
@@ -1,100 +1,56 @@
 use anyhow::Result;
-use clap::crate_version;
 use serde::Serialize;
 
-#[derive(Debug, Serialize)]
-struct Statistics {
-    total_reads: usize,
-    passing_reads: usize,
-    fraction_passing: f64,
-    num_filtered_1: usize,
-    num_filtered_2: usize,
-    num_filtered_3: usize,
-    num_filtered_4: usize,
+#[derive(Debug, Default, Serialize, Copy, Clone)]
+pub struct Statistics {
+    pub total_reads: usize,
+    pub passing_reads: usize,
+    pub fraction_passing: f64,
+    pub num_filtered_1: usize,
+    pub num_filtered_2: usize,
+    pub num_filtered_3: usize,
+    pub num_filtered_4: usize,
+}
+impl Statistics {
+    pub fn new() -> Self {
+        Self::default()
+    }
+    pub fn calculate_fraction_passing(&mut self) {
+        self.fraction_passing = self.passing_reads as f64 / self.total_reads as f64;
+    }
 }
 
 #[derive(Debug, Serialize)]
-struct Timing {
-    timestamp: String,
-    elapsed_time: f64,
+pub struct Timing {
+    pub timestamp: String,
+    pub elapsed_time: f64,
 }
 
 #[derive(Debug, Serialize)]
-struct FileIO {
-    readpath_r1: String,
-    readpath_r2: String,
-    writepath_r1: String,
-    writepath_r2: String,
+pub struct FileIO {
+    pub readpath_r1: String,
+    pub readpath_r2: String,
+    pub writepath_r1: String,
+    pub writepath_r2: String,
 }
 
 #[derive(Debug, Serialize)]
-struct Parameters {
-    offset: usize,
-    umi_len: usize,
-    exact_matching: bool,
-    pipspeak_version: String,
+pub struct Parameters {
+    pub offset: usize,
+    pub umi_len: usize,
+    pub exact_matching: bool,
+    pub pipspeak_version: String,
 }
 
 #[derive(Debug, Serialize)]
 /// A struct to hold the information about the run
 pub struct Log {
-    parameters: Parameters,
-    file_io: FileIO,
-    statistics: Statistics,
-    timing: Timing,
+    pub parameters: Parameters,
+    pub file_io: FileIO,
+    pub statistics: Statistics,
+    pub timing: Timing,
 }
 impl Log {
-    pub fn new(
-        total_reads: usize,
-        passing_reads: usize,
-        num_filtered_1: usize,
-        num_filtered_2: usize,
-        num_filtered_3: usize,
-        num_filtered_4: usize,
-        readpath_r1: String,
-        readpath_r2: String,
-        writepath_r1: String,
-        writepath_r2: String,
-        timestamp: String,
-        elapsed_time: f64,
-        offset: usize,
-        umi_len: usize,
-        exact_matching: bool,
-    ) -> Self {
-        let pipspeak_version = crate_version!().to_owned();
-        let fraction_passing = passing_reads as f64 / total_reads as f64;
-        let statistics = Statistics {
-            total_reads,
-            passing_reads,
-            fraction_passing,
-            num_filtered_1,
-            num_filtered_2,
-            num_filtered_3,
-            num_filtered_4,
-        };
-        let file_io = FileIO {
-            readpath_r1,
-            readpath_r2,
-            writepath_r1,
-            writepath_r2,
-        };
-        let parameters = Parameters {
-            offset,
-            umi_len,
-            exact_matching,
-            pipspeak_version,
-        };
-        let timing = Timing {
-            timestamp,
-            elapsed_time,
-        };
-        Self {
-            parameters,
-            file_io,
-            statistics,
-            timing,
-        }
-    }
 
     pub fn stderr(&self) -> Result<()> {
         let yaml = serde_yaml::to_string(&self)?;
--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ use clap::Parser;
 use cli::Cli;
 use config::Config;
 use flate2::{write::GzEncoder, Compression};
-use fxread::initialize_reader;
-use log::Log;
+use fxread::{initialize_reader, FastxRead, Record};
+use log::{Log, Statistics, Timing, Parameters, FileIO};
 use std::{fs::File, io::Write, time::Instant};
 
 /// Writes a record to a gzip fastq file
@@ -25,35 +25,26 @@ fn write_to_fastq<W: Write>(writer: &mut W, id: &[u8], seq: &[u8], qual: &[u8]) 
     Ok(())
 }
 
-fn main() -> Result<()> {
-    let args = Cli::parse();
-    let config = Config::from_file(&args.config, args.exact)?;
-    let r1 = initialize_reader(&args.r1)?;
-    let r2 = initialize_reader(&args.r2)?;
-
-    let r1_filename = args.prefix.clone() + "_R1.fq.gz";
-    let r2_filename = args.prefix.clone() + "_R2.fq.gz";
-    let log_filename = args.prefix.clone() + "_log.yaml";
-
-    let mut num_filtered_1 = 0;
-    let mut num_filtered_2 = 0;
-    let mut num_filtered_3 = 0;
-    let mut num_filtered_4 = 0;
-    let mut total_reads = 0;
-    let mut passing_reads = 0;
-    let start_time = Instant::now();
-    let timestamp = Local::now().to_string();
-
+fn parse_records(
+    r1: Box<dyn FastxRead<Item = Record>>,
+    r2: Box<dyn FastxRead<Item = Record>>,
+    r1_out: &mut GzEncoder<File>,
+    r2_out: &mut GzEncoder<File>,
+    config: &Config,
+    offset: usize,
+    umi_len: usize,
+) -> Result<Statistics> {
+    let mut statistics = Statistics::new();
     let record_iter = r1
         .zip(r2)
-        .inspect(|_| total_reads += 1)
+        .inspect(|_| statistics.total_reads += 1)
         .filter_map(|(rec1, rec2)| {
             if let Some((pos, b1_idx)) =
-                config.match_subsequence(rec1.seq(), 0, 0, Some(args.offset))
+                config.match_subsequence(rec1.seq(), 0, 0, Some(offset))
             {
                 Some((rec1, rec2, pos, b1_idx))
             } else {
-                num_filtered_1 += 1;
+                statistics.num_filtered_1 += 1;
                 None
             }
         })
@@ -61,7 +52,7 @@ fn main() -> Result<()> {
             if let Some((new_pos, b2_idx)) = config.match_subsequence(rec1.seq(), 1, pos, None) {
                 Some((rec1, rec2, pos + new_pos, b1_idx, b2_idx))
             } else {
-                num_filtered_2 += 1;
+                statistics.num_filtered_2 += 1;
                 None
             }
         })
@@ -69,28 +60,28 @@ fn main() -> Result<()> {
             if let Some((new_pos, b3_idx)) = config.match_subsequence(&rec1.seq(), 2, pos, None) {
                 Some((rec1, rec2, pos + new_pos, b1_idx, b2_idx, b3_idx))
             } else {
-                num_filtered_3 += 1;
+                statistics.num_filtered_3 += 1;
                 None
             }
         })
         .filter_map(|(rec1, rec2, pos, b1_idx, b2_idx, b3_idx)| {
             if let Some((new_pos, b4_idx)) = config.match_subsequence(&rec1.seq(), 3, pos, None) {
-                passing_reads += 1;
+                statistics.passing_reads += 1;
                 Some((rec1, rec2, pos + new_pos, b1_idx, b2_idx, b3_idx, b4_idx))
             } else {
-                num_filtered_4 += 1;
+                statistics.num_filtered_4 += 1;
                 None
             }
         })
         .map(|(rec1, rec2, pos, b1_idx, b2_idx, b3_idx, b4_idx)| {
-            let umi = &rec1.seq()[pos..pos + args.umi_len];
+            let umi = &rec1.seq()[pos..pos + umi_len];
             (
                 b1_idx,
                 b2_idx,
                 b3_idx,
                 b4_idx,
                 umi.to_vec(),
-                pos + args.umi_len,
+                pos + umi_len,
                 rec1,
                 rec2,
             )
@@ -102,33 +93,67 @@ fn main() -> Result<()> {
             (construct_seq, construct_qual, rec1, rec2)
         });
 
+    for (c_seq, c_qual, rec1, rec2) in record_iter {
+        write_to_fastq(r1_out, rec1.id(), &c_seq, &c_qual)?;
+        write_to_fastq(r2_out, rec2.id(), rec2.seq(), rec2.qual().unwrap())?;
+    }
+
+    statistics.calculate_fraction_passing();
+    Ok(statistics)
+}
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+    let config = Config::from_file(&args.config, args.exact)?;
+    let r1 = initialize_reader(&args.r1)?;
+    let r2 = initialize_reader(&args.r2)?;
+
+    let r1_filename = args.prefix.clone() + "_R1.fq.gz";
+    let r2_filename = args.prefix.clone() + "_R2.fq.gz";
+    let log_filename = args.prefix.clone() + "_log.yaml";
+
     let mut r1_writer = GzEncoder::new(File::create(&r1_filename)?, Compression::default());
     let mut r2_writer = GzEncoder::new(File::create(&r2_filename)?, Compression::default());
 
-    for (construct_seq, construct_qual, rec1, rec2) in record_iter {
-        write_to_fastq(&mut r1_writer, rec1.id(), &construct_seq, &construct_qual)?;
-        write_to_fastq(&mut r2_writer, rec2.id(), rec2.seq(), rec2.qual().unwrap())?;
-    }
+    let timestamp = Local::now().to_string();
+    let start_time = Instant::now();
 
-    let elapsed = start_time.elapsed().as_secs_f64();
-
-    let log = Log::new(
-        total_reads,
-        passing_reads,
-        num_filtered_1,
-        num_filtered_2,
-        num_filtered_3,
-        num_filtered_4,
-        args.r1.clone(),
-        args.r2.clone(),
-        r1_filename.clone(),
-        r2_filename.clone(),
-        timestamp,
-        elapsed,
+    let statistics = parse_records(
+        r1,
+        r2,
+        &mut r1_writer,
+        &mut r2_writer,
+        &config,
         args.offset,
         args.umi_len,
-        args.exact,
-    );
+    )?;
+
+    let elapsed_time = start_time.elapsed().as_secs_f64();
+    let timing = Timing {
+        timestamp,
+        elapsed_time,
+    };
+
+    let parameters = Parameters {
+        offset: args.offset,
+        umi_len: args.umi_len,
+        exact_matching: args.exact,
+        pipspeak_version: env!("CARGO_PKG_VERSION").to_string(),
+    };
+
+    let file_io = FileIO {
+        readpath_r1: args.r1,
+        readpath_r2: args.r2,
+        writepath_r1: r1_filename,
+        writepath_r2: r2_filename,
+    };
+
+    let log = Log {
+        parameters,
+        timing,
+        statistics,
+        file_io,
+    };
 
     if !args.quiet {
         log.stderr()?;
